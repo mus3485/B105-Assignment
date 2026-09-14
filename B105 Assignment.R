bb78465ab3ec9fab9dea7017489d0cf4df015624
Muhammad Mustafa Raza <mustafaraza6618@gmail.com>
2026-09-14 11:19:04 +0200

    question3 visualization completed and did anova hypothesis test as had four diffrent shipping categories

--- B105 Assignment.R
+++ B105 Assignment.R
@@ -50,3 +50,32 @@ ggplot(region_sales, aes(x = reorder(Region, Total_Sales), y = Total_Sales, fill
   ) +
   theme_minimal()
 
+ship_mode_sales <- train_clean %>%
+  group_by(Ship.Mode) %>%
+  summarise(
+    Total_Sales = sum(Sales, na.rm = TRUE),
+    Average_Sales = mean(Sales, na.rm = TRUE),
+    Order_Count = n()
+  ) %>%
+  arrange(desc(Total_Sales))
+print(ship_mode_sales)
+
+ggplot(ship_mode_sales, aes(x = reorder(Ship.Mode, Total_Sales), y = Total_Sales, fill = Ship.Mode)) +
+  geom_col() +
+  scale_y_continuous(labels = scales::comma) +
+  labs(
+    title = "Total Sales Revenue by Shipping Mode",
+    x = "Shipping Mode",
+    y = "Total Sales ($)"
+  ) +
+  theme_minimal()
+
+anova_model <- aov(Sales ~ Ship.Mode, data = train_clean)
+
+# View the ANOVA summary table (p-values, F-statistics)
+summary(anova_model)
+
+# Check model assumptions (Normality of residuals / diagnostics)
+par(mfrow = c(2, 2))
+plot(anova_model)
+par(mfrow = c(1, 1))
